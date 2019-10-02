1e0ec89dbb9129005b3d2b6eccd1efe9d1af407c
Tintindrx66 <drx89820704@gmail.com>
2019-10-02 11:12:15 -0400

    load python file

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,1 +1,17 @@
-
+def BinarySearch(nums[],target):
+    if nums==null||nums.length==0:
+        return -1
+    start,end=0,nums[].length-1
+    while start+1<end:
+        mid=start+(end-start)/2
+        if nums[mid]==target:
+            return mid
+        else if nums[mid]<target:
+            start=mid;
+        else
+            end=mid;
+    if nums[start]==target:
+        return start
+    if nums[end]==target:
+        return end
+    return -1
