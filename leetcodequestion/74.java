c0f523b6cdac3271c178894611a2020545068ce1
Tintindrx66 <drx89820704@gmail.com>
2019-10-04 15:41:13 -0400

    I submitted first solution but I got a problem that is time limit exceeded.

---
+++ leetcodequestion/74.java
@@ -0,0 +1,60 @@
+//Search a 2D matrix
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //Approach1:
+        /*if(matrix==null||matrix.length==0){
+            return false;
+        }
+        
+        for(int[] nums:matrix){
+            if(nums==null||nums.length==0){return false;}
+            int start=0;
+            int end=nums.length-1;
+            while(start+1<end){
+                int mid=start+(end-start)/2;
+                if(nums[mid]==target){
+                    return true;
+                }else if(nums[mid]<target){
+                    start=mid;
+                }else{
+                    end=mid;
+                }
+            }
+            if(nums[start]==target||nums[end]==target){
+                return true;
+            }
+        }
+        return false;*/
+        
+        //Approach2:
+        if(matrix==null || matrix.length==0 || matrix[0].length==0){
+            return false;
+        }
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for(int i = 0; i < row; i ++){
+            if(matrix[i][0] <= target && matrix[i][col-1] >= target)
+                return binarySearch(matrix[i], target);
+        }
+        return false;
+    }
+    
+    public boolean binarySearch(int[] array, int target){
+        int left = 0, right = array.length-1;
+        while(left+1 <= right){
+            int midCol = left+(right+left)/2;
+            if(array[midCol] == target){
+                return true;
+            }
+            else if(array[midCol] > target){
+                right = midCol;
+            }else{
+                left = midCol;
+            }
+            if(array[left]==target||array[right]==target){
+                return true;
+            }    
+        }
+        return false;
+    }
+}
