3db6188df3b95900ec548ae0e6369eb395a95315
Tintindrx66 <drx89820704@gmail.com>
2019-10-04 13:09:07 -0400

    binary tree type

--- leetcodequestion/563.java
+++ leetcodequestion/563.java
@@ -1,6 +1,7 @@
 /*Binary Tree Tilt*/
 /*First of all:consider special case when root is null. Then, use divide and conquer to search
 left and right subtree. Finally computed tilt and return result*/
+//TreeNode
 public class TreeNode{
     int val;
     TreeNode left;
@@ -10,7 +11,8 @@ public class TreeNode{
 }
 public int findTilt(TreeNode root) {
         int tilt=0;
-        if(root==null)return 0;
+        if(root==null)return 0;//exit of recursion
+    
         int leftpath=findTilt(root.left);
         int rightpath=findTilt(root.right);
         
