e8d2e7015ea38d9ed517a3e6d1a9840fae006c96
Ray Bian <rayleigh0328@gmail.com>
2019-10-05 00:20:21 -0400

    add comments to 872 solution

--- leetcodequestion/872.java
+++ leetcodequestion/872.java
@@ -1,4 +1,18 @@
 /*The idea is to find nodes whose left and right is null, comparing values of leaves on both trees to check whether both trees are leaf-similar */
+
+// COMMENT FROM RAY
+// I think you draw a wrong conclusion which your code based on 
+// Note that leaf sequence is the same does not mean sequence from left child is the same and sequence from the right child is the same
+// for example. the first tree have empty right sequence, the right tree have empty left sequqnce
+// however, they can have the same leaf sequence
+//     1       1
+//    /         \
+//   2           2
+//  /             \ 
+// 3               3
+//
+// try construct the leaf sequence for both trees first, and then compare the sequence.
+
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         if(root1==null||root2==null){
