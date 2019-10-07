7ca7b6540d0f6114a99ccddfff7e6dfbf9f72c10
Tintindrx66 <drx89820704@gmail.com>
2019-10-06 21:13:43 -0400

    Two ways to solve it.

--- leetcodequestion/1217.java
+++ leetcodequestion/1217.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minCostToMoveChips(int[] chips) {
         
-         /*int cost=0;
+                 /*int cost=0;
         int[] costarr=new int[chips.length];
         if(chips==null||chips.length==0||chips.length==1){
             return 0;
@@ -23,7 +23,10 @@ class Solution {
         Arrays.sort(costarr);
         return costarr[0];*/
         
-        int odd=0;
+        
+        
+        //Approach1:
+        /*int odd=0;
         int even=0;
         for(int i=0;i<chips.length;i++){
             if(chips[i]%2==0){
@@ -32,6 +35,29 @@ class Solution {
                 odd++;
             }
         }
-        return Math.min(odd,even);
+        return Math.min(odd,even);*/
+        
+        //Approach2:
+        int cost=0;
+        int[] count=new int[chips.length];
+        int min=Integer.MAX_VALUE;
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=0;i<chips.length;i++){
+            count[i]++;//count number of each element into a new array
+            for(int j=i+1;j<count.length;j++){
+                if(count[j]%2==0){
+                    cost++;
+                    cost*=count[j];
+                }
+                          
+            }
+            arr.add(cost);//store each cost into ArrayList
+            cost=0;      
+        }
+        for(int i=0;i<arr.size();i++){
+            min=Math.min(min, arr.get(i));//aquire a minimum value
+        }
+        
+        return min;
     }
 }
