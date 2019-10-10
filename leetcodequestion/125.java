c863ff76e58f0b06c5e87af4408fc9a08d7d798d
Tintindrx66 <drx89820704@gmail.com>
2019-10-09 23:22:23 -0400

    ETL error

--- leetcodequestion/125.java
+++ leetcodequestion/125.java
@@ -1,15 +1,31 @@
-# use stack push and pop
 class Solution {
     public boolean isPalindrome(String s) {
         
-        Stack<Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            stack.push(s.charAt(i));
+        String q="";
+        String p="";
+        
+        char[] ch=s.toLowerCase().toCharArray();
+        for(int i=0;i<ch.length;i++){
+            char c=ch[i];
+            if((c>='a'&&c<='z')||(c>='0'&&c<='9')){
+                q+=c;
+            
+            }
         }
-        String p=" ";
+        
+      
+        
+        
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<q.length();i++){
+            stack.push(q.charAt(i));
+        }
+        
+        
         while(!stack.isEmpty()){
             p+=stack.pop();
         }
-        return s.equals(p);
+        return q.equals(p);
+      
     }
 }
