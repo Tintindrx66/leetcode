bd38dd36fb808483bfa8ed3a5c61a9b068353c29
Tintindrx66 <drx89820704@gmail.com>
2019-10-12 20:38:47 -0400

    valid palindrome

--- leetcodequestion/125.java
+++ leetcodequestion/125.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isPalindrome(String s) {
         
         String q="";
-        String p="";
+        //String p="";
         
         char[] ch=s.toLowerCase().toCharArray();
         for(int i=0;i<ch.length;i++){
@@ -12,20 +12,31 @@ class Solution {
             
             }
         }
-        
+        char[] cha=new char[q.length()];
       
         
-        
+        //approach1
         Stack<Character> stack=new Stack<>();
         for(int i=0;i<q.length();i++){
             stack.push(q.charAt(i));
+            
         }
+        for(int i=0;i<q.length();i++){
+            
+            cha[i]=stack.pop();
+            
+        }
+        p=String.valueOf(cha);
         
         
-        while(!stack.isEmpty()){
-            p+=stack.pop();
-        }
         return q.equals(p);
+        //approach2:
+        /*for(int i=0;i<q.length()/2;++i){
+            if(q.charAt(i)!=q.charAt(q.length()-i-1)){
+                return false;
+            }
+        }
+        return true;*/
       
     }
 }
