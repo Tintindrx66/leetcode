b10fd13d69664f948174209a02caabc2e32b9e17
Tintindrx66 <drx89820704@gmail.com>
2019-11-20 00:00:53 -0500

    Single number2
    use hashmap to store each values and counts. return the key if the value is equal to 1. The time complexity is log(n) but the space complexity log(n).

--- leetcodequestion/137.java
+++ leetcodequestion/137.java
@@ -3,15 +3,15 @@ class Solution {
         if(nums==null||nums.length==0){return 0;}
         Map<Integer, Integer> map=new HashMap<>();
         for(int i: nums){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i], 1);
+            if(!map.containsKey(i)){
+                map.put(i, 1);
             }else{
-                map.put(nums[i], map.get(nums[i])+1);
+                map.put(i, map.get(i)+1);
             }
         }
         for(Integer key: map.keySet()){
-            if(key==1){
-                return map.get(key);
+            if(map.get(key)==1){
+                return key;
             }
         }
         return 0;
